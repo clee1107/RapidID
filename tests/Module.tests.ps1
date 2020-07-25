abe63619daa233cba0341eabe25357f73775af4b
clee <clee@shakopee.k12.mn.us>
2020-07-25 07:54:14 -0500

    Created Pester test for Module, confirms loads, confirms desired public cmdlets present.

---
+++ tests/Module.tests.ps1
@@ -0,0 +1,27 @@
+$buildPath = $PSScriptRoot.replace('tests','build')
+$moduleName = "RapidID"
+$modulePath = "$buildPath\$moduleName"
+
+Describe "RapidID Module" {
+    Context 'Strict mode' {
+        Set-StrictMode -Version latest
+        Import-Module $ModulePath -Force
+        It 'Should load' {
+            $Module = Get-Module $ModuleName
+            $Module.Name | Should -Be $ModuleName
+            $Commands = $Module.ExportedCommands.Keys
+            $Commands -contains 'Get-RIDGroupLogic' | Should -Be True
+            $Commands -contains 'Remove-RIDGroupLogic' | Should -Be True
+            $Commands -contains 'Set-RIDGroupLogic' | Should -Be True
+            $Commands -contains 'Test-RIDGroupLogic' | Should -Be True
+        }
+    }
+}
+
+<# Generate Commands tests
+$String = @"
+`$Commands -contains '<>' | Should -Be $True
+"@
+
+Get-Command -Module RapidID | foreach {$string -replace '\<\>',$_.name}
+#>
