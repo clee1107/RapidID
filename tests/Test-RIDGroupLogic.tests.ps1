98675015c2c2067a333426c04eb22d71b9157603
clee <clee@shakopee.k12.mn.us>
2020-07-27 09:53:41 -0500

    Pester test with Mocked AD functions

---
+++ tests/Test-RIDGroupLogic.tests.ps1
@@ -0,0 +1,43 @@
+$FunctionName =  "Test-RIDGroupLogic"
+Describe 'Test-RIDGroupLogic' -Tag "Test-RIDGroupLogic" {
+    BeforeAll {
+        Mock Get-ADDomainController {
+            [PSCustomObject]@{
+                HostName = 'dc1.domain.com'
+            }
+        }
+    }
+    Context 'Group Exists, Filter set' {
+        BeforeAll {
+            Mock Get-ADGroup {
+                [PSCustomObject]@{
+                    name                     = 'GroupWithLogic'
+                    idautoGroupIncludeFilter = "(&(employeetype=staff))"
+                }
+            }
+        }
+        It 'target group idautologicIncludeFilter set, should be True' {
+            (Test-RIDGroupLogic -Identity "GroupWithLogic") | Should -BeTrue
+        }
+    }
+    Context 'Group Exists, No Filter ($NULL)' {
+        BeforeAll {
+            Mock Get-ADGroup {
+                [PSCustomObject]@{
+                    name                     = 'GroupWithLogic'
+                    idautoGroupIncludeFilter = $Null
+                }
+            }
+        }
+        It 'target group idautologicIncludeFilter not set ($NULL), should be False' {
+            (Test-RIDGroupLogic -Identity "GroupWithLogic") | Should -BeFalse
+        }
+    }
+    Context 'Code Quality check - ScriptAnalyzer' {
+        $Path = Get-ChildItem -Path .\src -Filter "*$FunctionName*" -Recurse | Select-Object -ExpandProperty FullName
+        $ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path $Path -Severity Warning
+        It 'Should not return any violations' {
+            $ScriptAnalyzerResults | Should -BeNullOrEmpty
+        }
+    }
+}
