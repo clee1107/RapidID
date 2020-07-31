9e0b85bdc20ec2ff646a1483e133408849c01362
clee <clee@shakopee.k12.mn.us>
2020-07-31 11:14:26 -0500

    Pester Test for Get-RIDGroupLogic added check for Help ad Mandatory parameters.

--- tests/Get-RIDGroupLogic.tests.ps1
+++ tests/Get-RIDGroupLogic.tests.ps1
@@ -1,12 +1,32 @@
 $FunctionName =  "Get-RIDGroupLogic"
 Describe 'Get-RIDGroupLogic' -Tag "Get-RIDGroupLogic" {
-    Context 'Get' {
-        BeforeAll {
-            Mock Get-ADDomainController {
-                [PSCustomObject]@{
-                    HostName = 'dc1.domain.com'
-                }
+    BeforeAll {
+        Mock Get-ADDomainController {
+            [PSCustomObject]@{
+                HostName = 'dc1.domain.com'
             }
+        }
+    }
+    Context 'Parameter & Help  Checks' {
+		Set-StrictMode -Version latest
+		BeforeAll {
+			$helpinfo = Get-Help Get-RIDGroupLogic
+		}
+
+		It 'should have Identity as mandatory' {
+			{Get-RIDGroupLogic -Identity $null} | Should -Throw
+		}
+		It 'should have Help along with Description and examples' {
+			$helpinfo | Should -Not -BeNullOrEmpty
+		}
+		It 'should have Help containing examples, Description, and Details' {
+			$helpinfo.examples | Should -Not -BeNullOrEmpty  # should have examples
+            $helpinfo.Details | Should -Not -BeNullOrEmpty   # Should have Details in the Help
+            $helpinfo.Description | Should -Not -BeNullOrEmpty # Should have a Descriptiong for the Function
+		}
+	}
+    Context 'function checks' {
+        BeforeAll {
             Mock Get-ADGroup {
                 [PSCustomObject]@{
                     name                     = 'PesterGroupWithLogic'
@@ -14,17 +34,17 @@ Describe 'Get-RIDGroupLogic' -Tag "Get-RIDGroupLogic" {
                 }
             }
         }
-        It 'Get-RIDGroupLogic should run Get-ADGroup' {
-            Get-RIDGroupLogic -Identity "Doesn'tMatter"
+        It 'should call Get-ADGroup' {
+            Get-RIDGroupLogic -Identity "PesterGroupWithLogic"
 
             Should -Invoke Get-ADGroup -Times 1 -Scope It
         }
-        It 'Get-RIDGroupLogic target group idautologicIncludeFilter should not be null' {
-            (Get-RIDGroupLogic -Identity "Doesn'tMatter").idautoGroupIncludeFilter | Should -Not -BeNullOrEmpty
+        It 'should return idautologicIncludeFilter value' {
+            (Get-RIDGroupLogic -Identity "PesterGroupWithLogic").idautoGroupIncludeFilter | Should -BeTrue
         }
 
     }
-    Context 'ScriptAnalyzer' {
+    Context 'Code Quality check - ScriptAnalyzer' {
         $Path = Get-ChildItem -Path .\src -Filter "*$FunctionName*" -Recurse | Select-Object -ExpandProperty FullName
         $ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path $Path -Severity Warning
         It 'Should not return any violations' {
